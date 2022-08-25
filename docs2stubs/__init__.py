e68ac1ed68e4ed2cf237164a7276a62e09740698
Graham Wheeler <gram@geekraver.com>
2022-08-25 14:51:12 -0700

    Completed the basic stub generator with no type enhancements.

--- docs2stubs/__init__.py
+++ docs2stubs/__init__.py
@@ -2,11 +2,12 @@
 docs2stubs.
 
 Usage:
-  docs2stubs <module>
+  docs2stubs [--strip-defaults] <module>
   docs2stubs -h | --help
   docs2stubs --version
 
 Options:
+  --strip-defaults       Replace parameter default values with ...
   <module>               The module to generate stubs for (e.g. matplotlib.pyplot).
 
 The package that has the module needs to be installed in the environment from which
@@ -16,11 +17,12 @@ you are running docs2stubs.
 __version__ = '0.1'
 
 from docopt import docopt, DocoptExit
-from .docs2stubs import docs2stubs
+from .docs2stubs import stub_module
 
 
 def main():
     arguments = docopt(__doc__, version=__version__)
-    module = arguments['<package>']
-    docs2stubs(module)
+    module = arguments['<module>']
+    strip_defaults = arguments['--strip-defaults']
+    stub_module(module, strip_defaults)
     
--- docs2stubs/docs2stubs.py
+++ docs2stubs/docs2stubs.py
@@ -1,16 +1,27 @@
-from abc import ABC, abstractmethod
 import glob
 import importlib
 import inspect
-import sys
+import os
 import libcst as cst
 
 
 class StubbingTransformer(cst.CSTTransformer):
 
+    def __init__(self, strip_defaults=False):
+        self.strip_defaults = strip_defaults
+        self.in_simple_statement_count = 0
+        self.in_class_count = 0
+        self.in_function_count = 0
+
+    def leave_Assign(self, original_node: cst.Assign, updated_node: cst.Assign) -> cst.CSTNode:
+        return updated_node.with_changes(value=cst.parse_expression('...'))
+
+    def leave_AnnAssign(self, original_node: cst.Assign, updated_node: cst.Assign) -> cst.CSTNode:
+        return updated_node.with_changes(value=cst.parse_expression('...'))
+    
     def leave_Param(self, original_node: cst.Param, updated_node: cst.Param) -> cst.CSTNode:
         """ Remove default values, replace with ..."""
-        if original_node.default is not None:
+        if self.strip_defaults and original_node.default is not None:
             return updated_node.with_changes(default=cst.parse_expression('...'))
         return updated_node
 
@@ -18,8 +29,35 @@ class StubbingTransformer(cst.CSTTransformer):
         """ Remove function bodies """
         return updated_node.with_changes(body=cst.parse_statement('...'))
      
+    @staticmethod
+    def keep_simple_statement(node: cst.CSTNode)-> bool:
+        return isinstance(node, cst.Assign) or \
+               isinstance(node, cst.AnnAssign) or \
+               isinstance(node, cst.Import) or \
+               isinstance(node, cst.ImportFrom)
 
-def stub_module(m):
+    def leave_SimpleStatementLine(self, original_node: cst.SimpleStatementLine, updated_node: cst.SimpleStatementLine) -> cst.CSTNode:
+        newbody = [node for node in updated_node.body \
+            if StubbingTransformer.keep_simple_statement(node)]
+        return updated_node.with_changes(body=newbody)
+
+        return super().leave_SimpleStatementLine(original_node, updated_node)
+ 
+    @staticmethod
+    def keep_module_statement(node: cst.CSTNode)-> bool:
+        return isinstance(node, cst.ClassDef) or \
+               isinstance(node, cst.FunctionDef) or \
+               isinstance(node, cst.SimpleStatementLine)
+
+    def leave_Module(self, original_node: cst.Module, updated_node: cst.Module) -> cst.Module:
+        """ Remove everything from the body that is not an import,
+            class def, function def, or assignment.
+        """
+        newbody = [node for node in updated_node.body \
+            if StubbingTransformer.keep_module_statement(node)]
+        return updated_node.with_changes(body=newbody)
+
+def stub_module(m: str, strip_defaults: bool = False):
     try:
         mod = importlib.import_module(m)
         print(f'Imported module {m} for patching')
@@ -47,24 +85,19 @@ def stub_module(m):
             print(f'Failed to parse {file}: {e}')
             continue
         try:
-            patcher = StubbingTransformer()
+            patcher = StubbingTransformer(strip_defaults=strip_defaults)
             modified = cstree.visit(patcher)
-            if modified.code == cstree.code:
-                print(f'File {file} is unchanged')
-            else:
-                print(f'Patched file {file}')
-                target = 'typings/' + file[file.find('/site-packages/')+15] + 'i'
-                print(f'Writing {target}')
-                with open(target, 'w') as f:
-                    f.write(modified.code)
         except:# Exception as e:
             # Note: I know that e is undefined below; this actually lets me
             # successfully see the stack trace from the original excception
             # as traceback.print_exc() was not working for me.
             print(f'Failed to patch file {file}: {e}')
+            continue
 
-
-def docs2stubs(module: str):
-    print(f'Patching module {module}')
-    stub_module(module)
+        target = 'typings/' + file[file.find('/site-packages/')+15:] + 'i'
+        folder = target[:target.rfind('/')]
+        os.makedirs(folder, exist_ok=True)
+        with open(target, 'w') as f:
+            f.write(modified.code)
+        print(f'Stubbed file {file}')
 
